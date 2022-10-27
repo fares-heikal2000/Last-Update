62b8aa7ec20729e3fd4009b3e7c2ec1127f1e7f9
fares-heikal2000 <farisomarheikal@gmail.com>
2022-10-27 15:44:56 +0200

    New Update

--- Tradex Project/Js/main.js
+++ Tradex Project/Js/main.js
@@ -1,3 +1,4 @@
+// Owl Carousel
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel(
         {
@@ -20,3 +21,44 @@ $(document).ready(function(){
             }
     });
 });
+
+// Button Scrool
+window.addEventListener("scroll" , function() {
+    if(document.documentElement.scrollTop > 300) {
+        this.document.querySelector(".scroll").style.visibility = "visible";
+        this.document.querySelector(".scroll").style.opacity = "1";
+    } else {
+        document.querySelector(".scroll").style.visibility = "hidden";
+        this.document.querySelector(".scroll").style.opacity = "0";
+    }
+})
+
+
+// counter
+elements = document.querySelectorAll(".countWant");
+for (i = 0; i < elements.length; i++) {
+  elements[i].innerText = "0";
+}
+
+let x = false;
+function interval(tag, end, increase, time){
+    let N = 0;
+    let set = setInterval(() => {
+        if (N === end || N > end){
+            clearInterval(set);
+        } 
+        tag.innerText = N
+        N += increase;
+    }, time)
+    x = true;
+}
+window.addEventListener("scroll" , function() {
+    if(document.documentElement.scrollTop > 365) {
+      if (x == false){
+          interval(elements[0], 8000, 50, 9);
+         interval(elements[1], 810, 5, 10);
+         interval(elements[2], 2000, 10 , 8);
+         interval(elements[3], 20, 1, 70);
+      }
+    }
+})
